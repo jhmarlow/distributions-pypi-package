437a16dd0121410fd2c4a75c8d5a636ff2e449bc
jhmarlow1 <jhmarlow1@gmail.com>
2020-03-25 20:31:05 +0000

    adding created package

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='udacityds_distributions_jacob',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['udacityds_distributions_jacob'],
+      zip_safe=False)
---
+++ udacityds_distributions_jacob/README.md
@@ -0,0 +1,8 @@
+# Distributions Package
+This was built as part of the udacity datascience NanoDegree.
+
+# Summary
+
+# Files
+
+# Installation
